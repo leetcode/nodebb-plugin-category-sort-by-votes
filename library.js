2b97cffa95fceb6d38e2c3f1ff7ec68a3ac03ffe
wktang <woonkhang@gmail.com>
2016-04-24 10:17:31 -0400

    Move filter to use lower level topics.get() which returns the votes data, so infinite scrolling works too.

--- library.js
+++ library.js
@@ -4,26 +4,18 @@ var topics = module.parent.require('./topics');
 
 var theme = {};
 
-function addPostData(params, templateData, callback) {
-    var tids = templateData.topics.map(function(topic) {
+theme.addTopicsVotesInCategory = function(params, callback) {
+    var tids = params.topics.map(function(topic) {
         return topic.tid;
     });
 
-    topics.getMainPosts(tids, templateData.uid, function(err, mainPosts) {
+    topics.getMainPosts(tids, params.uid, function(err, mainPosts) {
         mainPosts.forEach(function(post, index) {
-            templateData.topics[index].votes = post.votes;
+            params.topics[index].votes = post.votes;
         });
 
         callback(null, params);
     });
-}
-
-theme.addTopicsVotesInCategory = function(params, callback) {
-	addPostData(params, params, callback);
-};
-
-theme.addTopicsVotes = function(params, callback) {
-	addPostData(params, params.templateData, callback);
 };
 
 module.exports = theme;
